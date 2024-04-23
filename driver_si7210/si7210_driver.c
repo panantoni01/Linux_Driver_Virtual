53163472d200588601dd781f75bf8c0231f11135
Antoni Pokusiński <apokusinski@o2.pl>
2024-04-23 16:03:56 +0200

    driver_si7210: add driver and test app stubs

---
+++ driver_si7210/si7210_driver.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+
+static int __init si7210_init(void)
+{
+  printk(KERN_INFO "s7210_driver registration\n");
+  return 0;
+}
+
+static void __exit si7210_cleanup(void)
+{
+  printk(KERN_INFO "si7210_driver removal\n");
+}
+
+module_init(si7210_init);
+module_exit(si7210_cleanup);
+
+
+MODULE_LICENSE ("GPL");
+MODULE_AUTHOR ("Antoni Pokusinski");
+MODULE_DESCRIPTION ("si7210 driver");
