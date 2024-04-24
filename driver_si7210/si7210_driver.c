25d5828b003ce979cd25c24cee623f9e04c33a55
Antoni Pokusiński <apokusinski@o2.pl>
2024-04-24 22:09:38 +0200

    driver_si7210: fix format

--- driver_si7210/si7210_driver.c
+++ driver_si7210/si7210_driver.c
@@ -2,19 +2,18 @@
 
 static int __init si7210_init(void)
 {
-  printk(KERN_INFO "s7210_driver registration\n");
-  return 0;
+	printk(KERN_INFO "s7210_driver registration\n");
+	return 0;
 }
 
 static void __exit si7210_cleanup(void)
 {
-  printk(KERN_INFO "si7210_driver removal\n");
+	printk(KERN_INFO "si7210_driver removal\n");
 }
 
 module_init(si7210_init);
 module_exit(si7210_cleanup);
 
-
-MODULE_LICENSE ("GPL");
-MODULE_AUTHOR ("Antoni Pokusinski");
-MODULE_DESCRIPTION ("si7210 driver");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Antoni Pokusinski");
+MODULE_DESCRIPTION("si7210 driver");
