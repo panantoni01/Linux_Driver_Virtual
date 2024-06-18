9850e6653418db3a392ac2b1219547854d81d227
Antoni Pokusinski <apokusinski@o2.pl>
2024-06-18 19:01:51 +0200

    verify-format: format only explicit directories

--- verify-format.sh
+++ verify-format.sh
@@ -2,12 +2,14 @@
 
 TOOL=clang-format-14
 
+FORMAT_DIRS=$(echo -n driver_{calc,litex_gpio,si7021})
+
 MODIFY=-n
 if [ "$1" == "--fix" ]; then
   MODIFY=""
 fi
 
-$TOOL $MODIFY -style=file --Werror -i $(find driver_* -maxdepth 1 -name "*.[ch]")
+$TOOL $MODIFY -style=file --Werror -i $(find $FORMAT_DIRS -maxdepth 1 -name "*.[ch]" )
 
 if [ $? -ne 0 ]; then
   echo "Incorrect formatting!"
